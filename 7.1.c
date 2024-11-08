238e6c07e37845452c77fb9152a10cb1de845086
fqytgb <2407014111@st.nuc.edu.cn>
2024-11-08 14:16:04 +0800

    first commit

---
+++ 7.1.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+int main()
+{
+    char a, b;
+    scanf_s("%c", &a);
+    if (a = 'A') {
+        printf("apple");
+    }
+    else if (a = 'B') {
+        printf("bravo");
+    }
+    else if (a = 'C') {
+        printf("check");
+    }
+    else if (a = 'D') {
+        printf("delta");
+    }
+
+    else if (a = 'E') {
+        printf("even");
+    }
+    else if (a = 'F') {
+        printf("family");
+    }
+    else if (a = 'G') {
+        printf("give");
+    }
+    else if (a = 'H') {
+        printf("have");
+    }
+    else if (a = 'I') {
+        printf("include");
+    }
+    else if (a = 'J') {
+        printf("just");
+    }
+    else if (a = 'K') {
+        printf("know");
+    }
+    else if (a = 'L') {
+        printf("love");
+    }
+    else if (a = 'M') {
+        printf("more");
+    }
+    else if (a = 'N') {
+        printf("now");
+    }
+    else if (a = 'O') {
+        printf("one");
+    }
+    else if (a = 'P') {
+        printf("please");
+    }
+    else if (a = 'Q') {
+        printf("question");
+    }
+    else if (a = 'R') {
+        printf("right");
+    }
+    else if (a = 'S') {
+        printf("still");
+    }
+    else if (a = 'T') {
+        printf("there");
+    }
+    else if (a = 'U') {
+        printf("u");
+    }
+    else if (a = 'V') {
+        printf("very");
+    }
+    else if (a = 'W') {
+        printf("what");
+    }
+    else if (a = 'X') {
+        printf("x-ray");
+    }
+    else if (a = 'Y') {
+        printf("you");
+    }
+    else if (a = 'Z') {
+        printf("zero");
+    }
+    else {
+        printf("error!");
+    }
+    return 0;
+}
